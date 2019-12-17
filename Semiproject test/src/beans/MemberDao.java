1015c7f3faa0b67821c1a65c900857a8dd300bfa
seon30 <a90711651@gmail.com>
2019-12-17 16:22:00 +0900

    Dao연결

--- Semiproject test/src/beans/MemberDao.java
+++ Semiproject test/src/beans/MemberDao.java
@@ -1,5 +1,32 @@
 package beans;
 
-public class MemberDao {
+import java.sql.Connection;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class MemberDao {
+	
+	//자원을 참조하는 변수 생성(리모컨)
+	//private static DataSource source = context.xml의 자원 정보;
+	private static DataSource source;
+	static {
+		//source에 context.xml의 Resource 정보를 설정
+		//[1] 탐색 도구 생성
+		//[2] 도구를 이용하여 탐색 후 source에 대입
+		try {
+			InitialContext ctx = new InitialContext();//[1]
+			source = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");			
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//연결 메소드
+	public Connection getConnection() throws Exception{
+		return source.getConnection();
+	}
+	
 }
