bd829aea1a0baa4ebee3e03746bd417396d5888e
seon30 <a90711651@gmail.com>
2019-12-17 16:20:23 +0900

    MemberDto

--- Semiproject test/src/beans/MemberDto.java
+++ Semiproject test/src/beans/MemberDto.java
@@ -5,6 +5,41 @@ public class MemberDto {
 	private String id;
 	private String pw;
 	private String email;
+	private String grade;
+	private int point;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public MemberDto() {
+		super();
+	}
 
 
 }
