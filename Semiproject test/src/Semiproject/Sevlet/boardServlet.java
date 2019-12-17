0a9dd5c8b10113932a8fe0195654e2bd996271e8
huyunk11 <huyunk11@gmail.com>
2019-12-17 16:57:50 +0900

    메인검색->상세글 코딩만

---
+++ Semiproject test/src/Semiproject/Sevlet/boardServlet.java
@@ -0,0 +1,42 @@
+package Semiproject.Sevlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Semiproject.beans.BoardDao;
+import Semiproject.beans.BoardDto;
+
+@WebServlet(urlPatterns="/board/board.do")
+public class boardServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try {
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/plain; charset=UTF-8");	
+			
+			BoardDao boardDao = new BoardDao();
+			BoardDto boardDto =boardDao.	getDto(req.getParameter("keyword"));
+			
+			resp.getWriter().println("작성자: "+boardDto.getWriter());
+			resp.getWriter().println(""+boardDto.getTitle());
+			resp.getWriter().println(""+boardDto.getContent());
+
+			
+			// 페이지이동
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+	
+	
+}
---
+++ Semiproject test/src/Semiproject/beans/BoardDto.java
@@ -0,0 +1,69 @@
+package Semiproject.beans;
+
+public class BoardDto {
+	private int no;
+	private String writer, title, content, wdate, udate;
+	
+	public BoardDto(int no, String id, String title, String content, String wdate, String udate) {
+		super();
+		this.no = no;
+		this.writer = writer;
+		this.title = title;
+		this.content = content;
+		this.wdate = wdate;
+		this.udate = udate;
+	}
+
+	public BoardDto() {
+		super();
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+
+	public String getUdate() {
+		return udate;
+	}
+
+	public void setUdate(String udate) {
+		this.udate = udate;
+	}
+
+}
